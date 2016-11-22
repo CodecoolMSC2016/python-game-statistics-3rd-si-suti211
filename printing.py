c69772e29d4c977efcf6b93096a90c276f9fe91f
Bosnyák Imre Dávid <suti211@gmail.com>
2016-11-22 16:45:47 +0100

    Self testing in print.py

--- printing.py
+++ printing.py
@@ -1,5 +1,11 @@
 import reports
 # Printing functions
 
-print(reports.get_genres("game_stat.txt"))
-print(reports.sort_abc("game_stat.txt"))
+FILE_NAME = "game_stat.txt"
+print(reports.get_genres(FILE_NAME))
+print(reports.sort_abc(FILE_NAME))
+print(reports.count_games(FILE_NAME))
+print(reports.decide(FILE_NAME, 1997))
+print(reports.count_by_genre(FILE_NAME, "RPG"))
+print(reports.get_latest(FILE_NAME))
+print(reports.get_line_number_by_title(FILE_NAME, "World of Warcraft"))
