f7372df3c88927fee7877b1e2cbf62fbff7b6f6d
Bosnyák Imre Dávid <suti211@gmail.com>
2016-11-23 18:13:26 +0100

    improved export.py with user input, changed output from answers.txt to exports.txt

--- export.py
+++ export.py
@@ -8,20 +8,26 @@ def print_list(list, file):
 
 
 def main():
-    EXPORT_FILE = "answers.txt"
+    EXPORT_FILE = "exports.txt"
     FILE_NAME = "game_stat.txt"
+
+    release_in_year = int(input("Please enter a year to check: "))
+    game_count_by_genre = input("Which genre would you like to summarize?: ")
+    line_number_of = input(
+        "Which games' serial number would you like to get?: ")
+
     with open(EXPORT_FILE, "w") as file:
         file.write("There are {0:d} games in the file.\n".format(
             reports.count_games(FILE_NAME)))
 
-        file.write("Is there any game released in 1997?: {0:s}".format("yes.\n" if
-                                                                       reports.decide(FILE_NAME, 1997) else "no.\n"))
+        file.write("Is there any game released in {0:d}?: {1:s}".format(release_in_year, "yes.\n" if
+                                                                        reports.decide(FILE_NAME, 1997) else "no.\n"))
         file.write("Latest game release was: {0:s}.\n".format(
             reports.get_latest(FILE_NAME)))
-        file.write("There are {0:d} RPG games out there.\n".format(
-            reports.count_by_genre(FILE_NAME, "RPG")))
-        file.write("The serial number of Half-Life 2 is: {0:d}.\n".format(
-            reports.get_line_number_by_title(FILE_NAME, "Half-Life 2")))
+        file.write("There are {0:d} {1:s} games out there.\n".format(
+            reports.count_by_genre(FILE_NAME, game_count_by_genre), game_count_by_genre))
+        file.write("The serial number of {0:s} is: {1:d}.\n".format(line_number_of,
+                                                                    reports.get_line_number_by_title(FILE_NAME, line_number_of)))
 
         file.write("Available game genres:\n")
         genres = reports.get_genres(FILE_NAME)
