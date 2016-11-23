734643e8d237eeec9f43fb5639b1a4f1f9c41e26
Bosnyák Imre Dávid <suti211@gmail.com>
2016-11-23 13:02:33 +0100

    implemented get_most_played

--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,21 @@
 
 # Report functions
+
+
+def get_most_played(file_name="game_stat.txt"):
+    salesList = []
+    with open(file_name, "r") as file:
+        for lines in file:
+            salesList.append(lines.strip("\n").split("\t"))
+
+    # finding most sold game Index
+    maxi = 0
+    for i in range(len(salesList)):
+        if float(salesList[i][1]) > float(salesList[maxi][i]):
+            maxi = i
+
+    # if there is more games with the same sale values,
+    # we return the one with the lower index
+    if salesList.count(salesList[maxi][1] > 1):
+        return salesList[salesList.index(salesList[maxi][1])][0]
+    return salesList[maxi][0]
